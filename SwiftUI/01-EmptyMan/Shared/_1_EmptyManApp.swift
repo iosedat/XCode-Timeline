0f1ff817cf8b303f4a2842aa716697231b770769
Sedat Yildirim <wienertechfreak@gmail.com>
2021-05-25 22:46:24 +0200

    01-EmptMan

---
+++ SwiftUI/01-EmptyMan/Shared/_1_EmptyManApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_EmptyManApp.swift
+//  Shared
+//
+//  Created by shad0wBL0w on 25.05.21.
+//
+
+import SwiftUI
+
+@main
+struct _1_EmptyManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
